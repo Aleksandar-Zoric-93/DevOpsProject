315f4b0e4df906edf4e6d4a70ab1c7260579fd32
Aleksandar Zoric <aleksandarzoric93@gmail.com>
2017-02-19 18:57:38 +0000

    Added sample/skeleton code for the 4 main classes
    Added 4 classes;  Login,Registration and Welcome page. I then use the
    hide and show image button for the password field so we can hide and
    show entered password.  Also there is a connected SQLite database that
    adds some user information to the database and vice versa.

--- DevOps/app/src/main/java/ie/itt/devops/aleksandar/devopsproject/MainActivity.java
+++ DevOps/app/src/main/java/ie/itt/devops/aleksandar/devopsproject/MainActivity.java
@@ -1,7 +1,9 @@
 package ie.itt.devops.aleksandar.devopsproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login_sigin(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.log_in:
+                Intent i=new Intent(this,LogMeIn.class);
+                startActivityForResult(i, 500);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                break;
+
+            case R.id.sign_in:
+                i=new Intent(this,SignIn.class);
+                startActivityForResult(i, 500);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);;
+                break;
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }
---
+++ DevOps/app/src/main/java/ie/itt/devops/aleksandar/devopsproject/Welcome.java
@@ -0,0 +1,16 @@
+package ie.itt.devops.aleksandar.devopsproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hunter on 19/02/2017.
+ */
+
+public class Welcome extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+}
